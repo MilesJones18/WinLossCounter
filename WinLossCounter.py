bad7597a14439b1fd5d6379b57c207d7e192def5
Miles <gameghostjones@gmail.com>
2023-01-29 03:07:14 -0600

    Now allows for saving and loading previous sessions

--- WinLossCounter.py
+++ WinLossCounter.py
@@ -23,7 +23,8 @@ def button_add():
     global wins
     wins += 1
     if wins > 7:
-        wins = 0
+        reset()
+        label2.config(text=0)
     print(wins)
 
 
@@ -32,8 +33,8 @@ def button_addloss():
     global wins
     losses += 1
     if losses > 20:
-        wins = 0
-        losses = 0
+        reset()
+        label.config(text=0)
     print(losses)
 
 
@@ -53,12 +54,36 @@ def button_subtractloss():
     print(losses)
 
 
-def button_resets():
+def reset():
     global wins
     global losses
     wins = 0
     losses = 0
-    print(wins)
+
+
+def save():
+    global wins
+    global losses
+    file = open("saves.txt", "w")
+    current_wins = wins
+    current_losses = losses
+    L = [str(current_wins), '\n', str(current_losses)]
+    file.writelines(L)
+    file.close()
+
+
+def load():
+    global wins
+    global losses
+    file = open("saves.txt", "r")
+    content = file.readlines()
+    label.config(text=int(content[0]))
+    label2.config(text=int(content[1]))
+    wins = int(content[0])
+    losses = int(content[1])
+
+    print(content[0])
+    print(content[1])
 
 
 label = Label(root,
@@ -113,6 +138,18 @@ minusloss_button = ttk.Button(
     command=lambda: [button_subtractloss(), click2()]
 )
 
+save_button = ttk.Button(
+    root,
+    text="Save",
+    command=save
+)
+
+load_button = ttk.Button(
+    root,
+    text="Load",
+    command=load
+)
+
 label.place(x=110, y=80, height=50, width=100)
 label2.place(x=390, y=80, height=50, width=100)
 Wins.place(x=105, y=25, )
@@ -122,4 +159,6 @@ add_button.place(x=110, y=200, height=50, width=100)
 minus_button.place(x=110, y=250, height=50, width=100)
 addloss_button.place(x=390, y=200, height=50, width=100)
 minusloss_button.place(x=390, y=250, height=50, width=100)
+save_button.place(x=300, y=330, height=50, width=100)
+load_button.place(x=200, y=330, height=50, width=100)
 root.mainloop()
